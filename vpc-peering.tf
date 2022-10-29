c32ed53ff0eff79adad5006295fab9b4ec3ed9a1
Eunhak Lee <lee@enak.kr>
2022-10-29 17:53:26 +0900

    feat: accept peering connection with terraform

--- vpc-peering.tf
+++ vpc-peering.tf
@@ -17,4 +17,60 @@ resource "aws_vpc_peering_connection_accepter" "enak-ix-peering-acc" {
     provider = aws.singapore
     vpc_peering_connection_id = aws_vpc_peering_connection.enak-ix-peering.id
     auto_accept = true
+
+    depends_on = [
+      aws_vpc_peering_connection.enak-ix-peering
+    ]
+}
+
+resource "aws_vpc_peering_connection_options" "enak-ix-peering-seoul-opt" {
+    provider = aws.seoul
+    vpc_peering_connection_id = aws_vpc_peering_connection.enak-ix-peering.id
+    
+    requester {
+        allow_remote_vpc_dns_resolution = true
+    }
+
+    depends_on = [
+      aws_vpc_peering_connection_accepter.enak-ix-peering-acc
+    ]
+}
+
+resource "aws_vpc_peering_connection_options" "enak-ix-peering-singapore-opt" {
+    provider = aws.singapore
+    vpc_peering_connection_id = aws_vpc_peering_connection.enak-ix-peering.id
+    
+    accepter {
+        allow_remote_vpc_dns_resolution = true
+    }
+
+    depends_on = [
+      aws_vpc_peering_connection_accepter.enak-ix-peering-acc
+    ]
+}
+
+resource "aws_route" "seoul-ix-rt" {
+    provider = aws.seoul
+    route_table_id = aws_route_table.seoul-public-rt.id
+    destination_cidr_block = "20.0.0.0/16"
+    vpc_peering_connection_id = aws_vpc_peering_connection.enak-ix-peering.id
+
+    depends_on = [
+      aws_vpc_peering_connection_accepter.enak-ix-peering-acc
+    ]
+}
+
+resource "aws_route" "singapore-ix-rt" {
+    provider = aws.singapore
+    route_table_id = aws_route_table.singapore-public-rt.id
+    destination_cidr_block = "10.0.0.0/16"
+    vpc_peering_connection_id = aws_vpc_peering_connection.enak-ix-peering.id
+
+    depends_on = [
+      aws_vpc_peering_connection_accepter.enak-ix-peering-acc
+    ]
+}
+
+output "enak-ix-peering-connection" {
+    value = aws_vpc_peering_connection.enak-ix-peering.accept_status
 }
