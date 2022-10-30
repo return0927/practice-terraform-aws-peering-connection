d38d24af46291f1358227774f5d02bc940442698
Eunhak Lee <lee@enak.kr>
2022-10-30 17:08:24 +0900

    feat: connect NAT with private subnet via public one

--- singapore-network.tf
+++ singapore-network.tf
@@ -19,15 +19,39 @@ resource "aws_internet_gateway" "singapore-igw" {
     tags = { Name = "enak-ix-singapore-igw" }
 }
 
-## Private Subnet
-resource "aws_subnet" "singapore-private-subnet" {
+## Public Subnet
+resource "aws_subnet" "singapore-public-subnet" {
     provider = aws.singapore
-
-    vpc_id = aws_vpc.singapore-vpc.id
-    cidr_block = "20.0.1.0/24"
-    availability_zone = "ap-southeast-1a"
     
-    tags = { Name = "enak-ix-singapore-private-subnet" }
+    vpc_id = aws_vpc.singapore-vpc.id
+    cidr_block = "20.0.0.0/24"
+    availability_zone = "ap-southeast-1a"
+    map_public_ip_on_launch = true
+
+    tags = { Name = "enak-ix-singapore-public-subnet" }
+}
+
+resource "aws_route_table" "singapore-public-rt" {
+    provider = aws.singapore
+    
+    vpc_id = aws_vpc.singapore-vpc.id
+
+    tags = { Name = "enak-ix-singapore-public-rt" }
+}
+
+resource "aws_route" "singapore-public-rt" {
+    provider = aws.singapore
+    
+    route_table_id = aws_route_table.singapore-public-rt.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.singapore-igw.id
+}
+
+resource "aws_route_table_association" "singapore-public-rt-ass" {
+    provider = aws.singapore
+    
+    subnet_id = aws_subnet.singapore-public-subnet.id
+    route_table_id = aws_route_table.singapore-public-rt.id
 }
 
 resource "aws_eip" "singapore-nat-ip" {
@@ -46,9 +70,20 @@ resource "aws_nat_gateway" "singapore-nat-gw" {
     provider = aws.singapore
 
     allocation_id = aws_eip.singapore-nat-ip.id
-    subnet_id = aws_subnet.singapore-private-subnet.id
+    subnet_id = aws_subnet.singapore-public-subnet.id
 
     tags = { Name = "enak-ix-singapore-nat-gw" }
+}
+
+## Private Subnet
+resource "aws_subnet" "singapore-private-subnet" {
+    provider = aws.singapore
+
+    vpc_id = aws_vpc.singapore-vpc.id
+    cidr_block = "20.0.1.0/24"
+    availability_zone = "ap-southeast-1a"
+    
+    tags = { Name = "enak-ix-singapore-private-subnet" }
 }
 
 resource "aws_route_table" "singapore-private-rt" {
